bff498998085e36246d3d4516be585fafb694d2b
webfokka <sreen111222333@gmail.com>
2026-01-08 02:15:08 +0200

    Delete mine/js directory

--- mine/js/lostpass.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('.lostpass__button').addEventListener('click', event => {
-    setTimeout( () => {
-        document.querySelector(".lostpass__warning").style.display = "block";
-    }, 750)
-})
